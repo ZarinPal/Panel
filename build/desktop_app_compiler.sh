1e3f9b12b4b11fc9c94a2c248cbac1192dea2659
RTLer <hooman@rtler.com>
2017-05-22 18:17:36 +0430

    fix on .travis.yml

--- build/desktop_app_compiler.sh
+++ build/desktop_app_compiler.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+cp index-web.html public/index.html
+cp package.json public/package.json
+
 brew install wine --without-x11
 brew install mono
 brew install gnu-tar graphicsmagick xz
 brew install rpm
 
-npm run compile
+npm run compile --projectDir=public
+
+
+rm -rf dist/github
+rm -rf dist/linux-unpacked
+rm -rf dist/mac
+rm -rf dist/win-unpacked
+rm -rf dist/latest.yml
--- compiler.sh
+++ compiler.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
+git config --global user.email "ZarinPal@Travis"
+git config --global user.name "ZarinPal@Travis"
+git config --global push.default simple
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 .ssh-key # this key should have push access
+ssh-add .ssh-key
+git clone -b release-web git@github.com:ZarinPal/Panel.git public
+apt-get install yarn
+yarn install
+npm run production
+cp -R ./assets ./public/
+cp -R ./index-web.html ./public/index.html
+cd public
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
-    git config --global user.email "ZarinPal@Travis"
-    git config --global user.name "ZarinPal@Travis"
-    git config --global push.default simple
-    eval "$(ssh-agent -s)" #start the ssh agent
-    chmod 600 .ssh-key # this key should have push access
-    ssh-add .ssh-key
-    git clone -b release-web git@github.com:ZarinPal/Panel.git public
-    apt-get install yarn
-    yarn install
-    npm run production
-    cp -R ./assets ./public/
-    cp -R ./index-web.html ./public/index.html
-    cd public
     git add --all .
     git commit -m ":credit_card::tada:"
     git push origin HEAD:release-web
