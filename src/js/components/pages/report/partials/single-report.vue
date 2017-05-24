b86f57ad8d4b775ace7320a1fdca7d7ac68d78af
Masoud Lotfi <lion.masoud1@gmail.com>
2017-05-24 12:54:12 +0430

    test

--- src/js/components/pages/report/partials/single-report.vue
+++ src/js/components/pages/report/partials/single-report.vue
@@ -11,8 +11,14 @@ div
             span.transaction-fields-title {{report.outgo_amount | persianNumbers}}
         div.col-lg-2.col-md-2.col-sm-3.hidden-xs.ta-center
             span.transaction-fields-title {{report.outgo_count | persianNumbers}}
+        div.cal-heatmap
 </template>
+<script type="text/javascript" src="//d3js.org/d3.v3.min.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/cal-heatmap/3.3.10/cal-heatmap.min.js"></script>
+<link rel="stylesheet" href="//cdn.jsdelivr.net/cal-heatmap/3.3.10/cal-heatmap.css" />
 <script>
+    var cal = new CalHeatMap();
+    cal.init({});
     export default {
         name:'pages-report-partials-single-report',
         props:['report'],
