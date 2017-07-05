e36723fa7b36f15608cca1bad81e92617582749b
Amin <aminnazari55@gmail.com>
2017-07-05 10:42:58 +0430

    Request money manually users amount

--- src/js/components/pages/requestـmoney/partials/new_request_money.vue
+++ src/js/components/pages/requestـmoney/partials/new_request_money.vue
@@ -20,19 +20,11 @@
                             label(:for="user.zp")
                                 span.circle-checkbox
 
-                div.footer
-                    div.row
-                        div.col-xs.ta-right.no-margin
-                            span.user-counter.persian-num(v-if="checkUsers.length") {{checkUsers.length + ' ' + $i18n.t('requestMoney.personSelected')}}
-
-                        div.col-xs.no-margin
-                            button.btn.success.pull-left(v-ripple="" v-if="checkUsers.length" @click="nextStep") {{$i18n.t('requestMoney.nextStep')}}
-
             <!--Step 2-->
-            div.nav-request-type.ta-right(v-if="step == 2")
+            div.nav-request-type.ta-right(v-else-if="step == 2")
                 div.nav-selected-avatars
-                    img.avatar(v-for="(user, index) in checkUsers" v-if="index <= 4" :src="user.avatar")
-                    span.remain-user-number(v-if="checkUsers.length > 5") {{checkUsers.length - 5 |persianNumbers}}
+                    img.avatar(v-for="(user, index) in selectedUsers" v-if="index <= 4" :src="user.avatar")
+                    span.remain-user-number(v-if="checkUsers.length > 5") {{checkUsers.length - 5 |persianNumbers}}+
 
                 div.top-text {{ $i18n.t('requestMoney.chooseOneOfTheFollowingMethods') }}
                 div.row.method-box(:class="{'active' : requestType == 'Auto'}")
@@ -54,13 +46,44 @@
                         label(for="requestTypeManually")
                             span.circle-checkbox
 
-                div.footer
-                    div.row
-                        div.col-xs.ta-right.no-margin
-                            span.prev-link-text(@click="prevStep") {{$i18n.t('requestMoney.prevStep')}}
 
-                        div.col-xs.no-margin
-                            button.btn.success.pull-left(v-ripple=""  @click="nextStep") {{$i18n.t('requestMoney.nextStep')}}
+            <!--Step 3 Auto Division-->
+            div.auto-division(v-else-if="step == 3 && requestType == 'Auto'")
+                div.nav-request-amount
+                    span دنگ هر نفر
+                    span {{ autoPersonAmount | numberFormat | persianNumbers }}
+                    span {{ $i18n.t('transaction.toman') }}
+
+                div.row
+                    input(type="text" v-model="requestAmount" placeholder="مبلغ" @keyup="calcAutoRequestAmount")
+
+                div.row
+                    input(type="checkbox" id="shareRequestChk" v-model="shareRequestWithMe" @change="calcAutoRequestAmount")
+                    label(for="shareRequestChk")
+                        span
+                        |{{ $i18n.t('requestMoney.iAmPartOfThisRequest') }}
+
+            <!--Step 3 Manually Division-->
+            div(v-else-if="step == 3 && requestType == 'Manually'")
+                div(v-for="(user, index) in selectedUsers")
+                    div {{user.name}}
+                    div.row
+                        span {{ $i18n.t('requestMoney.sum') }}
+                        span {{ manuallyTotalAmount | numberFormat | persianNumbers}}
+                        span {{ $i18n.t('transaction.toman') }}
+
+                    input(type="text" placeholder="مبلغ" v-model="selectedUsers[index].amount" @keyup="calcManuallyTotalAmount")
+
+
+            div.footer
+                div.row
+                    div.col-xs.ta-right.no-margin
+                        span.prev-link-text(v-if="step > 1" @click="prevStep") {{$i18n.t('requestMoney.prevStep')}}
+                        span.user-counter.persian-num(v-if="step === 1") {{checkUsers.length + ' ' + $i18n.t('requestMoney.personSelected')}}
+
+                    div.col-xs.no-margin
+                        button.btn.success.pull-left(v-ripple="" @click="nextStep") {{$i18n.t('requestMoney.nextStep')}}
+
 
 </template>
 
@@ -73,10 +96,11 @@
         data() {
             return {
                 loading: false,
-                step: 1,
+                step: 1, //Select user, select pay method(auto, manually)
                 pageTitle: 'selectUsers',
                 closeModalContent: false,
                 checkUsers: [],
+                selectedUsers: [], //filter users by checkUsers NOTE:final object for users
                 users: {
                     0: {
                         'name': 'امین نظری',
@@ -103,8 +127,17 @@
                         'zp': 'zp.34345',
                         'avatar': '//gravatar.com/avatar/890daede1a83cb2def69216076e29400?r=g&d=mm'
                     },
+                    5: {
+                        'name': 'کاربر آخر',
+                        'zp': 'zp.45678896',
+                        'avatar': '//gravatar.com/avatar/890daede1a83cb2def69216076e29400?r=g&d=mm'
+                    },
                 },
                 requestType: 'Auto', //Auto, Manually
+                requestAmount: null,
+                shareRequestWithMe: false,
+                autoPersonAmount: 0,
+                manuallyTotalAmount: 0,
             }
         },
         props:['card'],
@@ -115,28 +148,6 @@
             validationErrors() {
                 return this.$store.state.alert.validationErrors;
             },
-            changeTitle() {
-                if(this.step === 1) {
-                    this.pageTitle = 'selectUsers';
-                }else if(this.step === 2) {
-                    this.pageTitle = 'requestType';
-
-                    let selectedUserObject = {};
-
-                    this.checkUsers.forEach(function (zpId) {
-                        let vm = this;
-                        selectedUserObject = _.find(this.users, function(user) {
-                            return user.zp === zpId;
-                        });
-                    });
-                    console.log(selectedUserObject);
-
-
-
-                } else if(this.step === 3) {
-                    this.pageTitle = 'requestType';
-                }
-            }
         },
         mounted() {
             this.closeModalContent = false
@@ -151,13 +162,55 @@
             nextStep() {
               this.step ++;
               this.changeTitle();
+                this.getSelectedUsers();
             },
             prevStep() {
                 this.step --;
                 this.changeTitle();
+                if(this.step === 2) {
+                    this.requestAmount = null;
+                    this.autoPersonAmount = 0;
+                }
             },
             searchUser(content) {
                 return _.some(this.users, _.unary(_.partialRight(_.includes, content)));
+            },
+            changeTitle() {
+                if(this.step === 1) {
+                    this.pageTitle = 'selectUsers';
+                }else if(this.step === 2) {
+                    this.pageTitle = 'requestType';
+                } else if(this.step === 3) {
+                    this.pageTitle = 'requestType';
+                }
+            },
+            /*** Get selected users data ***/
+            getSelectedUsers() {
+                if(this.step === 2){
+                    let vm = this;
+                    this.selectedUsers = _.filter(this.users, function(user) {
+                        return vm.checkUsers.indexOf(user.zp) !== -1;
+                    });
+                }
+            },
+            /*** Calculate total amount and dealing between users auto ***/
+            calcAutoRequestAmount() {
+                let usersCount = this.selectedUsers.length;
+                if(this.shareRequestWithMe) {
+                    this.autoPersonAmount = (this.requestAmount / (usersCount + 1)).toFixed(0);
+                } else {
+                    this.autoPersonAmount = (this.requestAmount / usersCount).toFixed(0);
+                }
+            },
+            /*** Sum every user manually amount ***/
+            calcManuallyTotalAmount() {
+                let vm = this;
+                this.manuallyTotalAmount = 0;
+                this.selectedUsers.forEach(function (user) {
+                    if(user.amount) {
+                        vm.manuallyTotalAmount = parseInt(vm.manuallyTotalAmount) + parseInt(user.amount);
+                    }
+                });
             }
         },
         components: {
