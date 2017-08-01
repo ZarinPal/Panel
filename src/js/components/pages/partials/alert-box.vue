84a0a7bf52ab86fee8596e5c0edf1fa416fca7d5
Amin <aminnazari55@gmail.com>
2017-08-01 10:55:47 +0430

    Edit user

---
+++ src/js/components/pages/partials/alert-box.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    div.warning-box-message
+        div.nav-title-box
+            span.icon-warning
+            span.title
+                slot(name="title")
+        div.body
+            slot(name="body")
+</template>
+
+
+<script>
+export default {
+    name: 'alert-box',
+};
+</script>
