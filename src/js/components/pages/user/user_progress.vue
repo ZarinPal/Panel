79e2c3c0a37d7134f8c38a898b27f56f25fceeef
Masoud A <amini141@gmail.com>
2017-12-11 17:30:10 +0330

    add User Progress

--- src/js/components/pages/user/user_progress.vue
+++ src/js/components/pages/user/user_progress.vue
@@ -2,18 +2,31 @@
     div.inner-content.nav-user-setting
         div.row.nav-page-header
             div.col-lg-6.col-md-6.col-sm-6.col-xs-6
-                p.page-title {{ $i18n.t('user.notificationSetting') }}
-                p.page-description {{ $i18n.t('user.notificationSettingDescription') }}
+                p.page-title {{ $i18n.t('user.userProgress') }}
+                p.page-description {{ $i18n.t('user.userProgressDescription') }}
+
+
+            div.col-xs-12.col-sm-12.col-md-12.col-lg-12.section.create-webservice
+                div.box.ta-center
+                    div
+                        img.user-avatar(:src="user.avatar")
+                    div
+                        span.user-name(v-if="userProgress") {{userProgress.points | persianNumbers}} {{ $i18n.t('user.percentOfComplate') }}
+
+
+        div.row.section
+            div.col-lg-4(v-for="(progress, progressKey) in userProgress" v-if="progressKey !== 'points'")
+                div.box.box-style
+                    i.icon-zp-dashboard.text-style
+                    span.text-style {{ $i18n.t('user.progress.' + progressKey) }}({{ progress.point }}%)
+                    span.done-prog(v-if="progress.check")
+                        span.pull-left.text-style انجام شده
+                    span.pending-prog(v-else)
+                        span.pull-left.text-style در انتظار
 
 
 
-        div.row.section.nav-add-address
-        div.col-xs.ta-left.no-left-margin.dir-ltr(v-if="userProgress.referrer.check")
-            span.text-value.persian-num هست
-        div.col-xs.ta-left.no-left-margin(v-else)
-            span.tag.pull-left نیست
-        div.box.full-width
-            span {{userProgress}}
+
 </template>
 
 
@@ -23,6 +36,9 @@
         computed: {
             userProgress(){
                 return this.$store.state.auth.user.user_progress;
+            },
+            user(){
+                return this.$store.state.auth.user;
             }
         }
     }
