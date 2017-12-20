3a07159da2d8a3034a9da1c6b5c1ce4edc6af762
Amin <aminnazari55@gmail.com>
2017-12-20 15:02:18 +0330

    Update vue-j-calendar and user progress 100 percent visible

--- src/js/components/pages/partials/modal.vue
+++ src/js/components/pages/partials/modal.vue
@@ -10,7 +10,7 @@
             enter-active-class="zoom-in"
             leave-active-class="zoom-out")
                 div.row.center-xs.modal-content(v-if="!closeModalContent" v-on:click.self="closeModal()")
-                    div.col-lg-5.col-md-5.col-sm-12.col-xs-12.modal-inner-content
+                    div.col-lg-4.col-md-5.col-sm-12.col-xs-12.modal-inner-content
                         div.header
                             span.icon-close(@click="closeModal()")
                             span.title
