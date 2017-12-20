2882567fa93f373616a1dd03ce274eef47b81627
Amin <aminnazari55@gmail.com>
2017-12-20 09:55:18 +0330

    Remove tab change on sidebar

--- src/js/components/pages/webservice/partials/ussd-activation.vue
+++ src/js/components/pages/webservice/partials/ussd-activation.vue
@@ -18,7 +18,7 @@
                         div.no-margin.last-label
                             span #
                         div.col-xs.no-margin
-                            input.input.ltr-input(v-focus="" v-validate="'required|numeric|max:5'" v-bind:data-vv-as="$i18n.t('webservice.ussd')" maxlength="5" type="text" v-model="ussd_id" name="ussd_id" :placeholder="$i18n.t('webservice.ussd')" autofocus tabindex="1")
+                            input.input.ltr-input(v-focus="" v-validate="'required|numeric|min:4|max:6'" v-bind:data-vv-as="$i18n.t('webservice.ussd')" maxlength="5" type="text" v-model="ussd_id" name="ussd_id" :placeholder="$i18n.t('webservice.ussd')" autofocus tabindex="1")
                         div.no-margin.first-label
                             span *788*97*
                     div.ta-right.full-width(v-if="validation('ussd_id')")
@@ -111,7 +111,7 @@
                     this.getPriceLoading = false;
                 });
             },
-            createWebservice() {
+            activeUssd() {
                 this.loading = true;
                 let ussdData = {
                     webservice: this.webservice.entity_id,
