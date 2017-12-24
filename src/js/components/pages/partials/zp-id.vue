7ac4530670ad3235f9b7d1b8f254800aa35905ee
Masoud A <amini141@gmail.com>
2017-12-24 15:55:19 +0330

    fix logo color on sidebar

--- src/js/components/pages/partials/zp-id.vue
+++ src/js/components/pages/partials/zp-id.vue
@@ -2,7 +2,7 @@
     div.nav-zp-id(v-bind:class="{'nav-zp-gold': user.level == '3', 'nav-zp-silver': user.level == '2', 'nav-zp-gray': user.level == '1', 'nav-zp-black': user.level == '-1', 'nav-zp-black': user.level == '0' }")
         span ZP. {{user.public_id}}
         span.zp-id-color(v-bind:class="{'zp-id-gold': user.level == '3', 'zp-id-silver': user.level == '2', 'zp-id-gray': user.level == '1' , 'zp-id-black': user.level == '-1', 'zp-id-black': user.level == '0' }")
-            small.icon-zarinpal
+            small.icon-zarinpal.zp-id-just-color
             span(v-if="user.level == '3'") {{$i18n.t('user.user_level_3')}}
             span(v-else-if="user.level == '2'") {{$i18n.t('user.user_level_2')}}
             span(v-else-if="user.level == '1'") {{$i18n.t('user.user_level_1')}}
