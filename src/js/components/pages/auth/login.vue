8a3c701e0bf4a062a236c6915e9005da3627bc70
Amin <aminnazari55@gmail.com>
2017-12-26 17:35:58 +0330

    Login to old panel

--- src/js/components/pages/auth/login.vue
+++ src/js/components/pages/auth/login.vue
@@ -95,12 +95,12 @@
             div.row.auth-privacy-footer
                 div.col-xs.ta-right
                     span.icon-prev
-                    router-link.link(v-bind:to="{ name: 'auth.register',params:{refererId:this.$route.params.refererId}}") {{$i18n.t('user.register')}}
+                    a.link(href="https://www.zarinpal.com/auth/login") {{$i18n.t('user.loginToOldPanel')}}
+                    <!--router-link.link(v-bind:to="{ name: 'auth.register',params:{refererId:this.$route.params.refererId}}") {{$i18n.t('user.register')}}-->
                 div.col-xs.ta-left
                     a.link(href="https://www.zarinpal.com/terms.html" target="blank") {{$i18n.t('user.rulesAndRegulations')}}
                     span.gap
                     a.link(href="https://www.zarinpal.com/policy.html" target="blank") {{$i18n.t('user.privacy')}}
-
 
     div(v-else)
         div.loading.ta-center
