817bc50a3cdd58be70f6fa276846a9afcbd6c1ed
Amin <aminnazari55@gmail.com>
2017-12-27 12:45:53 +0330

    Fix input number format problem

--- src/js/directives/mask.js
+++ src/js/directives/mask.js
@@ -1,31 +1,5 @@
 export default {
     bind(el, binding, vnode) {
-
-        let tokens = {
-            date: '-',
-            ip: '.'
-        };
-
-
-        el.addEventListener('keydown', function (event) {
-            /** ---------------
-             *      Date
-             ----------------**/
-            if (binding.value.type == 'date') {
-                let dateRegex = new RegExp('^\\d{4}\\' + tokens.date + '\\d{2}$');
-                if (event.keyCode !== 8) {
-                    let value = el.value;
-                    if (value.match(/^\d{4}$/) !== null) {
-                        el.value = value + tokens.date;
-                    } else if (value.match(dateRegex) !== null) {
-                        el.value = value + tokens.date;
-                    }
-                }
-            }
-
-        });
-
-
         /** ---------------
          *      Keyup
          ----------------**/
@@ -33,42 +7,12 @@ export default {
             /** ---------------
                     Money
              ----------------**/
-            if (binding.value.money) {
-                let key = window.event ? event.keyCode : event.which;
-                if (
-                    event.keyCode == 8 ||
-                    event.keyCode == 17 ||
-                    event.keyCode == 18 ||
-                    event.keyCode == 46 ||
-                    event.keyCode == 37 ||
-                    event.keyCode == 39 ||
-                    event.keyCode == 91 ||
-                    event.keyCode == 93
-                ) {
-                    let text = el.value.replace(/[^\d]/g, "");
-                    let number = text;
-                    if (number) {
-                        number = parseInt(text.replace(/\D/g, ''), 10);
-                    }
+            Vue.nextTick(function() {
+                if (binding.value.money && el.value) {
+                    let number = parseInt(el.value.replace(/\D/g,''),10);
                     el.value = number.toLocaleString();
-
-                    return true;
                 }
-                else if (key < 48 || key > 57) {
-                    //Letters
-                    event.preventDefault();
-                    return false;
-                } else {
-                    //Its mean key is valid number
-                    let text = el.value.replace(/[^\d]/g, "");
-                    let number = text;
-                    if (number) {
-                        number = parseInt(text.replace(/\D/g, ''), 10);
-                    }
-                    el.value = number.toLocaleString();
-                    return true;
-                }
-            }
-        });
+            });
+        }) ;
     }
 };
