ea3aef4e542852a5042f0f42cf2b9945da99cb3c
Masoud A <amini141@gmail.com>
2017-12-30 16:46:16 +0330

    fix show tickets

--- src/js/components/pages/ticket/pages/show.vue
+++ src/js/components/pages/ticket/pages/show.vue
@@ -10,17 +10,17 @@
 
             div.ver-line
             div.row.bottom-row
-                div.col-lg-4.col-md-4.col-sm-6.col-xs-12
+                div.col-lg-4.col-md-4.col-sm-6.col-xs-6
                     span.title {{ $i18n.t('ticket.updated') }} :
                     span.value.persian-num {{ticket.updated_at | fromNow }}
-                div.col-lg-4.col-md-4.col-sm-6.col-xs-12
+                div.col-lg-4.col-md-4.col-sm-6.col-xs-6
                     span.title {{ $i18n.t('ticket.created') }} :
                     span.value.persian-num {{ticket.created_at | fromNow }}
-                div.col-lg-3.col-md-3.col-sm-6.col-xs-12
+                div.col-lg-3.col-md-3.col-sm-6.col-xs-6
                     span.status.pull-left {{ $i18n.t('ticket.' + kebabCase(ticket.status)) }}
-                div.col-lg-1.col-md-1.col-sm-6.col-xs-12
-                    span.priority.pull-left {{ $i18n.t('ticket.' + kebabCase(ticket.priority)) }}
-                    button.btn.success.hidden-lg(v-on:click="closeReplies()") بازگشت
+                div.col-lg-1.col-md-1.col-sm-6.col-xs-6
+                    span.priority {{ $i18n.t('ticket.' + kebabCase(ticket.priority)) }}
+                    button.btn.success.hidden-lg.hidden-md.pull-left(v-on:click="closeReplies()") بازگشت
 
 
         div.replies
