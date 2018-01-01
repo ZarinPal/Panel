bd9514d80b703046622d86e10b27099adb4adaa5
Masoud A <amini141@gmail.com>
2018-01-01 17:08:53 +0330

    change ussd style to  vazir

--- src/js/components/pages/auth/login.vue
+++ src/js/components/pages/auth/login.vue
@@ -72,7 +72,7 @@
                                         span #
 
                                 div.col-xs
-                                    div.ussd-text.persian-num(v-if="ussdType =='Code'" @click="clipboardMessage(ussdCode)" v-clipboard="" v-bind:data-clipboard-text="ussdCode") {{ussdCode}}
+                                    div.ussd-text.vazir(v-if="ussdType =='Code'" @click="clipboardMessage(ussdCode)" v-clipboard="" v-bind:data-clipboard-text="ussdCode") {{ussdCode | persianNumbers}}
                                     img.qr-image(v-if="ussdType =='Qr'" v-bind:src="qrCodeSrc")
                             span.hidden-lg.hidden-md {{ $i18n.t('user.copyUssd') }}
 
