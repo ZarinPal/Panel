7368a9c1f05d3dfa6b4d799d4e602267fed98511
Masoud A <amini141@gmail.com>
2018-01-20 15:57:49 +0330

    remove validation method

--- src/js/components/pages/webservice/partials/create.vue
+++ src/js/components/pages/webservice/partials/create.vue
@@ -97,23 +97,6 @@
             this.$store.dispatch('app/getWebserviceCategories');
         },
         methods: {
-            validation(name) {
-                if (this.$store.state.alert.validationErrors[name]) {
-                    let element = _.find(this.$validator.fields.items, function (field) {
-                        return field.name == name;
-                    });
-                    this.errors.add(
-                        name,
-                        this.$validator.dictionary.container.fa.messages[this.$store.state.alert.validationErrors[name].rule](
-                            element.el.dataset.vvAs,
-                            this.$store.state.alert.validationErrors[name].params
-                        ),
-                        'api'
-                    );
-                    this.$store.state.alert.validationErrors[name] = false;
-                }
-                return this.errors.has(name);
-            },
             validateForm() {
                 this.$validator.validateAll({
                     site_name: this.site_name,
