4a133618d28908d9edf2e1291217a894223d0fbb
Masoud A <amini141@gmail.com>
2018-02-18 16:46:19 +0330

    add  html cod item to zarinlink

--- src/js/components/pages/easypay/partials/show_html_code.vue
+++ src/js/components/pages/easypay/partials/show_html_code.vue
@@ -5,13 +5,36 @@
             div.middle-xs.body
                 div.row.box-row
                     div.col-xs.ta-right
-                        span.label {{ $i18n.t('webservice.webserviceCode') }}
-
+                        span.label زرین‌لینک
                     div.col-xs.ta-left.no-margin
                         div.row.label-group.pull-left
                             div.col-xs.text.merchant-code
-                                input.txt-webservice-id(:id="'txtWebserviceId-'" onfocus="this.select();" v-bind:value="entity" readonly="readonly")
-                            div.icon.copy(@click="clipboardMessage(entity)" v-clipboard="entity" v-bind:data-clipboard-text="entity")
+                                input.txt-webservice-id(:id="'htmlCodeLink'" onfocus="this.select();" :value="zarinLinkUrl" readonly="readonly")
+                            div.icon.copy(@click="clipboardMessage(zarinLinkUrl)" v-clipboard="zarinLinkUrl" v-bind:data-clipboard-text="zarinLinkUrl")
+                div.row.box-row.padding-row
+                    div.col-xs.ta-right
+                        span.label کد HTML
+                    div.col-xs.ta-left.no-margin
+                        div.row.label-group.pull-left
+                            div.col-xs.text.merchant-code
+                                input.txt-webservice-id(:id="'htmlCode'" onfocus="this.select();" :value="zarinLinkHtml" readonly="readonly")
+                            div.icon.copy(@click="clipboardMessage(zarinLinkHtml)" v-clipboard="zarinLinkHtml" v-bind:data-clipboard-text="zarinLinkHtml")
+                div.row.box-row.padding-row
+                    div.col-xs.ta-right
+                        span.label کد دکمه ی پرداخت
+                    div.col-xs.ta-left.no-margin
+                        div.row.label-group.pull-left
+                            div.col-xs.text.merchant-code
+                                input.txt-webservice-id(:id="'htmlCodeButton'" onfocus="this.select();" :value="zarinLinkButton" readonly="readonly")
+                            div.icon.copy(@click="clipboardMessage(zarinLinkButton)" v-clipboard="zarinLinkButton" v-bind:data-clipboard-text="zarinLinkButton")
+                div.row.box-row.padding-row
+                    div.col-xs.ta-right
+                        span.label BBCode ویژه ی انجمن ها
+                    div.col-xs.ta-left.no-margin
+                        div.row.label-group.pull-left
+                            div.col-xs.text.merchant-code
+                                input.txt-webservice-id(:id="'htmlCodeBb'" onfocus="this.select();" :value="zarinLinkBb" readonly="readonly")
+                            div.icon.copy(@click="clipboardMessage(zarinLinkBb)" v-clipboard="zarinLinkBb" v-bind:data-clipboard-text="zarinLinkBb")
 
 
 
@@ -24,21 +47,38 @@
 
     export default {
         name: 'show-html',
-        data() {
-            return {
-                preLink: 'https://Zarinp.al/@',
-            }
-        },
         props: ['entity'],
         computed: {
-            user() {
-                return this.$store.state.auth.user;
+            zarinLinkUrl(){
+                return 'https://Zarinp.al/' + this.entity ;
+            },
+            zarinLinkHtml(){
+                return '<a target=\'_blank\' title=\'پرداخت آنلاین\' href=\'' + this.zarinLinkUrl + '\'>پرداخت آنلاین</a>';
+            },
+            zarinLinkButton(){
+                return '<a target=\'_blank\' title=\'پرداخت آنلاین\' href=\'' + this.zarinLinkUrl + '\'><img src=\'https://cdn.zarinpal.com/badges/easypay/logo1.png\'></a>';
+            },
+            zarinLinkBb(){
+                return '[url=' + this.zarinLinkUrl + ']پرداخت آنلاین[/url]';
             }
         },
         methods: {
             closeModal() {
                 this.$emit('closeModal');
             },
+            clipboardMessage(event) {
+                setTimeout(function () {
+                    let htmlCodeField = document.getElementById(event);
+                    if (htmlCodeField) {
+                        htmlCodeField.select();
+                    }
+                }, 10);
+                store.commit('flashMessage', {
+                    text: 'Copied',
+                    type: 'success',
+                    timeout: '1500'
+                });
+            }
         },
         components: {
             modal,
