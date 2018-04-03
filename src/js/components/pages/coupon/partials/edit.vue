a0e3f08d871a029bd9d20e57679d4a1833539337
Amin <aminnazari55@gmail.com>
2018-04-03 10:47:04 +0430

    Remove commented select box

--- src/js/components/pages/coupon/partials/edit.vue
+++ src/js/components/pages/coupon/partials/edit.vue
@@ -40,7 +40,6 @@
                                     | {{ $i18n.t('coupon.easypay') }}
                             div.col-lg-8.col-md-8.col-sm-12.col-xs-12
                                 zarin-link(v-on:select="selectedEasypay" v-if="easyPayId || type" v-bind:class="{'disable' : type == 'webservice', 'input-danger': errors.has('easypay_id')}" v-bind:selected="easyPayId" placeholder="انتخاب زرین‌لینک")
-                                <!--selectbox.selectbox.col-lg-12.col-md-12.col-sm-12.col-xs-12(v-on:select="selectedEasypay" v-bind:data="easypaySelection" v-if="easyPayId || type" v-bind:selected="easyPayId" v-bind:class="{'disable' : type == 'webservice' }"  placeholder="انتخاب زرین‌لینک"  tabindex="3")-->
                                 div.ta-right(v-if="validation('easypay_id')")
                                     span.text-danger {{ errors.first('easypay_id') }}
 
