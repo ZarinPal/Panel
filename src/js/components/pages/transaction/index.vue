4b0b1026890af44a245c784c654ce1ac519cffba
Masoud A <amini141@gmail.com>
2018-05-19 16:31:24 +0430

    [tranaction] remove tranaction type menu
    [panel][webservices][tranaction] if on dispage remove tranaction type
    menu

--- src/js/components/pages/transaction/index.vue
+++ src/js/components/pages/transaction/index.vue
@@ -54,13 +54,19 @@
     div.row.filter-row
       div.col-lg-4.col-md-3.col-sm-6.col-xs-6
         span.text(v-if="this.$route.params.type == 'purse'") {{$i18n.t('transaction.purseTransactionList')}}
+        span.text(v-if="this.$route.params.type == 'webservice'") {{$i18n.t('webservice.webserviceTransactionList')}}
+        span.text(v-if="this.$route.params.type == 'easypay'") {{$i18n.t('easypay.easypayTransactionList')}}
         span(v-for="purse in user.purses")
           span.purse-name(v-if="purse.purse == $route.params.id") {{purse.name}}
-      div.col-lg-4.col-md-3.col-sm-6.col-xs-6
+        span(v-for="webservice in user.webservices")
+          span.purse-name(v-if="webservice.entity_id == $route.params.id") {{webservice.name}}
+        span(v-for="easypay in user.easypays")
+          span.purse-name(v-if="easypay.entity_id == $route.params.id") {{easypay.title}}
+      div.col-xs
         a.btn.simple.pull-left(:href="'/rest/v3/transaction/excel.json?' + excelUrl")
           span.icon-zp-excel
           span {{$i18n.t('transaction.excelExport')}}
-      div.col-lg-4.col-md-3.col-sm-6.col-xs-6
+      div(v-if="this.$route.params.type == 'purse'").col-lg-4.col-md-3.col-sm-6.col-xs-6
         ul.select_item.pull-left(:class="{'disable-filter': loadingState.status}")
           li(v-ripple="" @click="applyGeneralFilter('all')" v-bind:class="{ active: generalFilter == 'all' }" ) {{$i18n.t('transaction.all')}}
           li(v-ripple="" @click="applyGeneralFilter('1')" v-bind:class="{ active: generalFilter == '1' }")  {{$i18n.t('transaction.deposit')}}
